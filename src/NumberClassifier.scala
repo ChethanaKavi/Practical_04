2634a07fc87911906c7d43a71ac1108f8c6d7373
ChethanaKavi <138358211+ChethanaKavi@users.noreply.github.com>
2024-07-08 05:10:51 +0530

    Update NumberClassifier.scala

--- src/NumberClassifier.scala
+++ src/NumberClassifier.scala
@@ -1,22 +1,15 @@
+import scala.io.StdIn.readInt
+
 object NumberClassifier {
-  def classifyNumber(number: Int): String = {
-    number match {
-      case n if n <= 0 => "Negative/Zero is input"
-      case n if n % 2 == 0 => "Even number is given"
-      case _ => "Odd number is given"
-    }
-  }
 
   def main(args: Array[String]): Unit = {
-    if (args.length != 1) {
-      println("Please provide exactly one integer as an argument.")
-    } else {
-      try {
-        val number = args(0).toInt
-        println(classifyNumber(number))
-      } catch {
-        case _: NumberFormatException => println("Invalid input. Please provide a valid integer.")
-      }
+    println("Enter an integer:")
+    val userInput = readInt()
+    
+    userInput match {
+      case x if x <= 0 => println("Negative/Zero is input")
+      case x if x % 2 == 0 => println("Even number is given")
+      case _ => println("Odd number is given")
     }
   }
 }
